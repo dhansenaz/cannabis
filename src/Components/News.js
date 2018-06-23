de10164695dd4fa3e5202226050a4d62cf1c5ffa
dhansenaz <dhansen@allthingsdot.com>
2018-06-23 09:50:32 -0700

    google news api with working links

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,53 +1,60 @@
-import React, {Component} from 'react'
-import axios from 'axios'
-import './news.css'
+import React, { Component } from "react";
+import axios from "axios";
+import "./news.css";
 
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('f1fbedfe5cdf4a0486e885999e3f9c27');
+const NewsAPI = require("newsapi");
+const newsapi = new NewsAPI("f1fbedfe5cdf4a0486e885999e3f9c27");
 
-class News extends Component{
-    constructor(props){
-        super(props)
+class News extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      news: []
+    };
 
-        this.state={
-            news:[]
-        }
+    this.getNews = this.getNews.bind(this);
+  }
 
-        this.getNews = this.getNews.bind(this)
-    }
-   
-     getNews(){
-        axios.get("https://newsapi.org/v2/everything?q=cannabis&apiKey=f1fbedfe5cdf4a0486e885999e3f9c27")
-        .then((response) => {
-          console.log(response.data.results)
-          this.setState ({news: response.data.articles})
-        })
+  getNews() {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?q=marijuana&apiKey=f1fbedfe5cdf4a0486e885999e3f9c27"
+      )
+      .then(response => {
+        console.log("response-----------------", response.data.results);
+        this.setState({ news: response.data.articles });
+      });
+  }
+  componentDidMount() {
+    this.getNews();
+  }
+  render() {
+    const people = this.state;
+    const googleNews = this.state.news.map(el => {
+      return (
+        <div className="section-a">
+          <h3>{el.description}</h3>
+          <a href={el.url} target="_blank"><img className="news-image" src={el.urlToImage} /></a>
+        </div>
+      );
+    });
+    return (
+      <div className="main">
+        
        
-
-      }
-      componentDidMount(){
-          this.getNews()
-      }
-    render(){
-       const people = this.state
-       const googleNews = this.state.news.map( el => {
-            return(
-                <div className='news-wrapper'>
-                <div>Title: {el.title}
-                    <img className='news-image'src={el.urlToImage}></img>
-                </div>
-                </div>
-                
-            )
-        })
-        return(
-            <div>
-               {googleNews}
-             {console.log('news-------------------------', googleNews)}
-            </div>
-           
-        )
-    }
+        <div className="section-a-wrapper">
+        <div className="banner">
+            <h1 className="banner-content1">Todays</h1>
+            <h1 className="banner-content2">Top</h1>
+            <h1 className="banner-content3">News</h1>
+          </div>
+          
+          {googleNews}
+          {console.log("news-------------------------", googleNews)}
+        </div>
+      </div>
+    );
+  }
 }
-export default News
+export default News;
