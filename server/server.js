b993d75a94099e25759d89dfe937d8bbbcc99f2b
dhansenaz <dhansen@allthingsdot.com>
2018-07-22 13:40:32 -0700

    server build

--- server/server.js
+++ server/server.js
@@ -7,8 +7,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use( express.static( `${__dirname}/../build` ) );
 
-
-
+const path = require('path')
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+})
 
 app.listen(PORT, () => console.log("You are running on port 4000"))
