5c3e5810fce47a1393b5c982ca95206e7fcf310b
dhansenaz <dhansen@allthingsdot.com>
2019-02-21 20:10:34 -0800

    nav bar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -8,29 +8,31 @@ const Nav = () => {
   return (
     <div>
       <nav>
-        <div className='logo'><img src={Logo} alt="logo" style={{ width: "50px" }} /></div>
-          <ul className="deskNav">
-            <li>
-              <Icon className="navIcon" type="laptop" />
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Icon className="navIcon" type="team" />
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Icon className="navIcon" type="environment" />
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-              <Icon className="navIcon" type="shopping-cart" />
-              <Link to="/">Shop</Link>
-            </li>
-            <li>
-              <Icon className="navIcon" type="bulb" />
-              <Link to="/blog">Blog</Link>
-            </li>
-          </ul>
+        <div className="logo">
+          <img src={Logo} alt="logo" style={{ width: "50px" }} />
+        </div>
+        <ul className="deskNav">
+          <li>
+            <Icon className="navIcon" type="laptop" />
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Icon className="navIcon" type="team" />
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Icon className="navIcon" type="environment" />
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Icon className="navIcon" type="shopping-cart" />
+            <Link to="/">Shop</Link>
+          </li>
+          <li>
+            <Icon className="navIcon" type="bulb" />
+            <Link to="/blog">Blog</Link>
+          </li>
+        </ul>
       </nav>
     </div>
   );
