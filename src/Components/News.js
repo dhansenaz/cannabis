be143d6d15d4aaa460ba9cc17220fabfc2419b8b
dhansenaz <dhansen@allthingsdot.com>
2019-02-21 20:13:47 -0800

    news

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./news.css";
-import NewsAPI from '../newsapi'
+import NewsAPI from "../newsapi";
 
 const newsapi = new NewsAPI("f1fbedfe5cdf4a0486e885999e3f9c27");
 
@@ -34,21 +34,20 @@ class News extends Component {
     const googleNews = this.state.news.map(el => {
       return (
         <div className="section-a">
-          <h3 >{el.title}</h3>
-          <a href={el.url} target="_blank"><img className="news-image" src={el.urlToImage} /></a>
+          <h3>{el.title}</h3>
+          <a href={el.url} target="_blank">
+            <img className="news-image" src={el.urlToImage} />
+          </a>
         </div>
       );
     });
     return (
       <div className="main">
-        
-       
         <div className="section-a-wrapper">
-        <div className="banner">
-        <h3>News</h3>
-            
+          <div className="banner">
+            <h3>News</h3>
           </div>
-          
+
           {googleNews}
           {console.log("news-------------------------", googleNews)}
         </div>
